8603e59df6795d736027079eb57786adf70ec9bc
José Carneiro <jose@light.pt>
2017-10-10 09:25:10 +0100

    Turned functionality into classes

--- test/scrape/setup.js
+++ test/scrape/setup.js
@@ -1,24 +1,13 @@
 'use strict';
 
-const { resolve } = require('path');
-const TwizScrape = require('./../../.');
+const Scrape = require('./../../.');
+const ScrapeYoutube = require('./../../lib/services/youtube');
 const config = require('./../config');
-const services = [
-  {
-    name: 'youtube',
-    data: config.google,
-    path: resolve(__dirname, './../../services/youtube')
-  }
-  // {
-  //   name: 'youtube',
-  //   data: config.google
-  // },
-  // 'medium',
-  // 'vimeo',
-  // 
-];
-const options = Object.assign(config, { debug: true }, { services });
 
-const scrape = new TwizScrape(options);
+const options = Object.assign(config, { debug: true });
 
-module.exports = scrape;
+module.exports = new Scrape(options, [
+  new ScrapeYoutube({
+    key: config.key
+  })
+]);
